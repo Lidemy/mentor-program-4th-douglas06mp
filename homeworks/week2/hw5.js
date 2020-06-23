5d428804326d215f9d19c46a6b6d257523d63c4c
douglas <douglas06mp@gmail.com>
2020-06-23 17:19:16 +0800

    finish week2 homework

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,21 @@
 function join(arr, concatStr) {
-  
+  if (arr.length === 0) return '';
+
+  let result = '';
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i] + (i === arr.length - 1 ? '' : concatStr);
+  }
+  return result;
 }
 
 function repeat(str, times) {
-  
+  let result = '';
+  while (times > 0) {
+    result += str;
+    times--;
+  }
+  return result;
 }
 
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+console.log(join(['a', 1, 'b', 2, 'c', 3], ','));
+console.log(repeat('yo', 3));
